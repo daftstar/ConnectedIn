304ad8169d8048db3741f33f348dce5798aaebff
Nik Daftary <nikdaftary@gmail.com>
2013-09-25 21:36:03 -0500

    code refactor complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 ConnectedIn::Application.routes.draw do
-root "friends#show_all"
 
-get "friends/will_leverett"       => "friends#show_will",      as: 'will_friends'
-get "friends/harsh_patel"         => "friends#show_harsh",     as: 'harsh_friends'
+root "profiles#show_all"
+get "/profiles"                    => "profiles#show_all"
 
 
-get "friends/drew_montgomery"     => "friends#show_drew",      as: 'drew_friends'
-get "friends/joe_marshall"        => "friends#show_joe",       as: 'joe_friends'
+get "/profiles/:profile_name" 			=> "profiles#show",	as: 'profile'
 
-get "/friends"                    => "friends#show_all"
+
+
+
+# get "profiles/will_leverett"       => "profiles#show_will",      as: 'will_profiles'
+# get "profiles/harsh_patel"         => "profiles#show_harsh",     as: 'harsh_profiles'
+
+
+# get "profiles/drew_montgomery"     => "profiles#show_drew",      as: 'drew_profiles'
+# get "profiles/joe_marshall"        => "profiles#show_joe",       as: 'joe_profiles'
+
 
 
 end
